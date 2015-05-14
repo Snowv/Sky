a3e8e0161ce8aa4e5da972374b1eac9479cef634
GHCody <pa.bonnel@inventati.org>
2015-05-14 15:55:20 +0200

    create remove

--- rvsh.sh
+++ rvsh.sh
@@ -7,6 +7,8 @@
 #git remote add bob https://github.com/Snowa/Sky-1.git
 #git fetch bob
 #git merge bob/master
+#git pull alice master
+#git push bob master
 
 programname=$0
 
@@ -19,28 +21,75 @@ function usage {
     echo "  -help       affiche de l'aide"
     exit 1
 }
+function usage_host {
+    echo "usage: >rvsh host [create|remove] [nom_machine] "
+    echo "  -connect    Le mode connect permet à un utilisateur de se connecter à une machine virtuelle"
+    echo "  -admin      Cette commande permet à l’administrateur de gérer la liste des machines connectées au réseau virtuel"
+    echo "              et la liste des utilisateurs. Lorsque que la commande admin est saisi il n'y a pas besoin de saisir le nom d'utilisateur"
+    echo "              et le nom de la machine"
+    echo "  -help       affiche de l'aide"
+}
+
 function admin {
     echo "Fonction administrateur"
     while [[ true ]]; do
-        read -p ">rvsh " cmd
-        if [ $cmd == "exit" ]; then
-            break;
-        fi
-    done
+        read -p ">rvsh " line
+        cmd=$(echo $line | cut -f1 -d" ")
+        case $cmd in
+            host )
+                action=$(echo $line | cut -f2 -d" ")
+                machineName=$(echo $line | cut -f3 -d" ")
+                host $action $machineName ;;
+afinger )
+;;
+users )
+;;
+* )
+echo "Commande inconnue"
+;;
+esac
+done
+}
+function host {
+    echo $*
+  if [[ $(echo $* | wc -w) = 2 && $1 == "create" ]]; then
+    createVirtualMachine $machineName
+elif [[ $(echo $* | wc -w) = 2 && $1 == "remove" ]]; then
+    removeVirtualMachine $machineName
+else
+    usage_host
+fi
 }
 function createVirtualMachine {
-
+    if [ ! -z $1  ]; then #test si la chaine est non vide
+        if [  ! -d  $1 ]; then
+            mkdir $machineName 
+            echo "Machine $1 créée"
+        else
+            echo "La machine virtuel est déjà créée"
+        fi
+    else 
+        echo "Nom de machine incorrect"
+    fi
 }
 function removeVirtualMachine {
-
+    if [ ! -z $1  ]; then #test si la chaine est non vide
+        if [ -d  $1 ]; then
+            rm -Rf $1 
+            echo "Machine $1 supprimée"
+        else
+            echo "La machine virtuel n'est pas encore créée"
+        fi
+    else 
+        echo "Nom de machine incorrect"
+    fi
 }
 function connect {
     echo "Focntion connect"
 }
-
-if [ $1 == "-admin" ]; then
+if [[ ! -z $1 && $1 == "-admin" ]]; then
     admin
-elif [ $1 == "-connect" ]; then
+elif [[ ! -z $1 && $1 == "-connect" ]]; then
     connect
 else
     usage
